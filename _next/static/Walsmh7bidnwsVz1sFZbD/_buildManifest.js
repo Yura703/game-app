75ca8e47da2b4ba8dd0548240a37fa6378a4c1f3
Юрий Лапицкий <C:\Users\lyuri\AppData\Roaming\The Bat!>
2025-12-10 10:59:04 +0300

    Deploy to GitHub Pages

--- _next/static/Walsmh7bidnwsVz1sFZbD/_buildManifest.js
+++ _next/static/Walsmh7bidnwsVz1sFZbD/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/game-app//_next/:path+",
-        "destination": "/game-app/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
